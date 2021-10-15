7b6860de37f2865e120432cb4dc0cd63667c533b
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-10-15 15:02:09 +0700

    Membuat Component Lain

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import Header from './../components/Header/index';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <header>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/blog"><a>Blog</a></Link></li>
-          <li><Link href="/users"><a>Users</a></Link></li>
-        </ul>
-      </header>
+      <Header />
+      <h1>Welcome Dede</h1>
     </div>
+
   )
 }
 
