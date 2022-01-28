4eb3d67b7060d226cf8bc5c9aaf00fa458e29a35
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-28 09:44:07 +0700

    Custom halaman 404 (Not Found Page)

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Custom404() {
+    return (
+        <div>
+            <h1 className="title-not-found">Ooops...</h1>
+            <h1 className="title-not-found">Halaman yang Anda cari tidak ada</h1>
+        </div>
+    );
+}
